83b660bc6b5a9ba3825b3db04da68b1bb7c0dc98
ryrylleee <delosreyeskarylle@gmail.com>
2025-11-20 21:40:59 +0800

    updated login with register page

--- app/src/main/java/com/example/loginformpractice/MainActivity.kt
+++ app/src/main/java/com/example/loginformpractice/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.loginformpractice
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val textSignup = findViewById<TextView>(R.id.text_signup)
+        textSignup.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
